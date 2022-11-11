b4c254d2afc2a5ccf3e79560b51df3384b2d27a3
Bruce Walter <2040147482@qq.com>
2022-11-11 17:12:37 +0800

    初始化商品模块

---
+++ scorpio-product/src/main/java/com/leslie/product/ProductApplication.java
@@ -0,0 +1,14 @@
+package com.leslie.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 20110
+ */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class,args);
+    }
+}
