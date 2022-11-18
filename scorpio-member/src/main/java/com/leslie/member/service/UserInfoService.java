43ee3336157af67031cacebf893470822ccb35cb
Bruce Walter <2040147482@qq.com>
2022-11-18 14:40:55 +0800

    重新逆向工程用户信息表

--- scorpio-member/src/main/java/com/leslie/member/service/UserInfoService.java
+++ scorpio-member/src/main/java/com/leslie/member/service/UserInfoService.java
@@ -1,13 +1,13 @@
 package com.leslie.member.service;
 
+import com.leslie.pojo.UserInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.leslie.member.pojo.UserInfo;
 
 /**
- * @author 20110
- * @description 针对表【tb_user_info(用户信息表)】的数据库操作Service
- * @createDate 2022-11-06 16:34:16
- */
+* @author 20110
+* @description 针对表【tb_user_info(用户信息表)】的数据库操作Service
+* @createDate 2022-11-18 14:36:50
+*/
 public interface UserInfoService extends IService<UserInfo> {
 
 }
--- scorpio-member/src/main/java/com/leslie/member/service/impl/UserInfoServiceImpl.java
+++ scorpio-member/src/main/java/com/leslie/member/service/impl/UserInfoServiceImpl.java
@@ -1,19 +1,19 @@
 package com.leslie.member.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.leslie.member.pojo.UserInfo;
-import com.leslie.member.mapper.UserInfoMapper;
+import com.leslie.pojo.UserInfo;
 import com.leslie.member.service.UserInfoService;
+import com.leslie.member.mapper.UserInfoMapper;
 import org.springframework.stereotype.Service;
 
 /**
- * @author 20110
- * @description 针对表【tb_user_info(用户信息表)】的数据库操作Service实现
- * @createDate 2022-11-06 16:34:16
- */
+* @author 20110
+* @description 针对表【tb_user_info(用户信息表)】的数据库操作Service实现
+* @createDate 2022-11-18 14:36:50
+*/
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo>
-        implements UserInfoService {
+    implements UserInfoService{
 
 }
 
