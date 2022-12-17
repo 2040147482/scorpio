f7743a063d866f03ec0a63c88a2e64ae36933666
Bruce Walter <2040147482@qq.com>
2022-12-17 17:41:30 +0800

    返回结果通用对象Result实现序列化

--- scorpio-commons-feign/src/main/java/com/leslie/utils/Result.java
+++ scorpio-commons-feign/src/main/java/com/leslie/utils/Result.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -14,7 +15,9 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Result {
+public class Result implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     /**
      * 成功状态码
